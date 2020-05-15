13a81a958eba29e81c851aafa25102f874c8a3ed
Yaroslav <maguryan.mail@gmail.com>
2020-05-15 13:05:20 +0300

    pulse_step_four

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,33 @@
-$(document).ready(function(){
+/* Tiny Slider */
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: true,
+  navPosition: "bottom",
+
+  responsive: {
+    991: {
+      nav: false,
+      
+    }
+  }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+}); 
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+}); 
+
+
+/* Jquery slick slider */
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
       speed: 800,
       //adaptiveHeight: true,
@@ -16,4 +45,4 @@ $(document).ready(function(){
     });
   });
 
-  
+   */
