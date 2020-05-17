5c1f96c9df472a67699ac01b69559d7b214602a2
Yaroslav <maguryan.mail@gmail.com>
2020-05-17 20:36:56 +0300

    pulse_v1.0

--- src/js/script.js
+++ src/js/script.js
@@ -149,11 +149,21 @@ $(document).ready(function(){
 		}
 	});
 
-	$("a[href^='#']").click(function(){
+	$("a[href='#up']").click(function(){
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
-});
+	});
+
+	$("a[href='#catalog']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+
+	/* Animation */
+	new WOW().init();
 
 });
 
